af98640d923b4d50641f2222e6f2e5f28b4b8edf
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-03-10 03:17:09 +0300

    classes

--- part1/build/js/main.js
+++ part1/build/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 // +++++++++++++++++++++++++++++++++++++++++++++ declaring variables ++++++++++++++++++++++++++++++++
-let username = 'ivan';
-let myName = 'ivan';
+let username = "ivan";
+let myName = "ivan";
 let anyVariable;
 let a = 3;
 let b = 13;
@@ -13,7 +13,7 @@ console.log(a * b);
 console.log(username);
 // +++++++++++++++++++++++++++++++++++++++++++++    functions ++++++++++++++++++++++++++++++++
 function sum(c, d) {
-    let answer = (c + d);
+    let answer = c + d;
     // console.log(answer);
     return answer;
 }
@@ -22,12 +22,22 @@ sum(2, 3);
 let stringArr = ["w", "i", "n"];
 stringArr.unshift("someone");
 console.log(stringArr);
-function personprinter({ firstname, secondname }) {
-    console.log(`${firstname} ${secondname}`);
-    return `${firstname} ${secondname}`;
-}
-let person = {
-    firstname: "ivan",
-    secondname: "mukisa"
+let arr = [];
+arr.push();
+let dev1 = {
+    name: "ivan",
+    active: true,
+    albums: [],
 };
-personprinter(person);
+function greetdeveloper(developer) {
+    console.log("hello " + developer.name);
+}
+greetdeveloper(dev1);
+/// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["A"] = 0] = "A";
+    Grade[Grade["B"] = 1] = "B";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["D"] = 3] = "D";
+})(Grade || (Grade = {}));
--- part1/src/main.ts
+++ part1/src/main.ts
@@ -1,58 +1,86 @@
 // +++++++++++++++++++++++++++++++++++++++++++++ declaring variables ++++++++++++++++++++++++++++++++
 
-
-
-let username = 'ivan'
-let myName = 'ivan'
-
+let username = "ivan";
+let myName = "ivan";
 let anyVariable: any;
-
-
 let a: number = 3;
 let b: number = 13;
-anyVariable = true
+anyVariable = true;
 let e: string;
 let f: string | number | boolean;
-e= "you are"
-
-
-
+e = "you are";
 console.log(a * b);
 console.log(username);
 
-
 // +++++++++++++++++++++++++++++++++++++++++++++    functions ++++++++++++++++++++++++++++++++
 
-
-
- function sum(c:number, d:number): number {
-  let answer = (c + d)
-   // console.log(answer);
-   return answer;
-  
+function sum(c: number, d: number): number {
+  let answer = c + d;
+  // console.log(answer);
+  return answer;
 }
-
-sum(2,3)
+sum(2, 3);
 
 // +++++++++++++++++++++++++++++++++++++++++++++ arrays sets and tuples  ++++++++++++++++++++++++++++++++
 
-
-let stringArr = ["w", "i", "n"]
-stringArr.unshift("someone")
-
+let stringArr = ["w", "i", "n"];
+stringArr.unshift("someone");
 console.log(stringArr);
+let arr: string[] = [];
+arr.push();
 
+// +++++++++++++++++++++++++++++++++++++++++objects something like models
 
-function personprinter({firstname, secondname}: {firstname:string, secondname: string}) {
-  console.log(`${firstname} ${secondname}`)
-  return `${firstname} ${secondname}`
+interface Developer {
+  name: string;
+  active: boolean;
+  albums: (string | number)[];
+}
 
+let dev1: Developer = {
+  name: "ivan",
+  active: true,
+  albums: [],
+};
+
+function greetdeveloper(developer: Developer) {
+  console.log("hello " + developer.name);
+}
+greetdeveloper(dev1);
+
+///  +++++++++++++++++++++++++++++++++++++++++ Enums
+enum Grade {
+  A,
+  B,
+  C,
+  D,
+}
+
+// +++++++++++++++++++++++++++++++++++++++++type assertions /type casting
+
+type one = string;
+type two = string | number;
+type three = "hello";
+
+let y: one = "hello";
+let x = y as two; // example of assertion
+let z = <three>"main";
+
+//+++++++++++++++++++++++++++++++++++++++++ classes
+
+class Coder {
+   name: String;
+  age: number;
+  location: string;
+
+  // constructor(public readonly name: String, private age: number, protected location: string) {
+
+  constructor( name: String, age: number, location: string) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
 }
 
 
-let person = {
-  firstname: "ivan",
-  secondname: "mukisa"
-}
-
-personprinter(person)
+let coder = new Coder("ivan", 23, "kajj")
