e1797f324599a4385473278b7c5bccbb6d769e40
mukisaivan <106663129+mukisaivan@users.noreply.github.com>
2024-03-11 21:59:28 +0300

    something

--- part1/build/js/main.js
+++ part1/build/js/main.js
@@ -33,7 +33,7 @@ function greetdeveloper(developer) {
     console.log("hello " + developer.name);
 }
 greetdeveloper(dev1);
-/// Enums
+///  +++++++++++++++++++++++++++++++++++++++++ Enums
 var Grade;
 (function (Grade) {
     Grade[Grade["A"] = 0] = "A";
@@ -41,3 +41,94 @@ var Grade;
     Grade[Grade["C"] = 2] = "C";
     Grade[Grade["D"] = 3] = "D";
 })(Grade || (Grade = {}));
+let y = "hello";
+let x = y; // example of assertion
+let z = "main";
+//+++++++++++++++++++++++++++++++++++++++++ classes
+class Coder {
+    // constructor(public readonly name: String, private age: number, protected location: string) {
+    constructor(name, age, location) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+}
+let coder = new Coder("ivan", 23, "kajj");
+class WebDev extends Coder {
+    constructor(name, age, location, laptopbrand) {
+        super(name, age, location);
+        this.name = name;
+        this.laptopbrand = laptopbrand;
+        this.laptopbrand = laptopbrand;
+    }
+}
+////////////////////////////////////////////////////////
+class Keyboardist {
+    play(actionn) {
+        return `${this.name} ${actionn} ${this.instrument}`;
+    }
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+}
+///////////////////////////////////////////////////////////
+const Page = new Keyboardist('mukisa', 'keyboard');
+console.log(Page.play('hits'));
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0; //static means that we can only call count after referencing this class forexample Peeps.count
+let John = new Peeps("John");
+let Lenon = new Peeps("John");
+let Bill = new Peeps("John");
+console.log(John.id);
+console.log(Peeps.count);
+///////////////////////////////////////////////////////////
+class Bands {
+    constructor() {
+        this.datastate = [];
+    }
+    get data() {
+        return this.datastate;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.datastate = value;
+            return;
+        }
+        else {
+            throw new Error("some values in this array are not strings");
+        }
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['goo', 'lee', 'goo']; // -------------->setter
+console.log(MyBands.data); // --------------> getter
+MyBands.data = [...MyBands.data, 'sisis'];
+console.log(MyBands.data);
+const todaysTransactions = {
+    Pizza: 10,
+    Books: 10,
+    Job: 20
+};
+let prop = 'Pizza';
+console.log(todaysTransactions[prop]);
+function todaysNet(transactions) {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+}
+console.log(todaysNet(todaysTransactions));
+//chaging the property of the object forexample 
+todaysTransactions.Pizza = 20;
+console.log(todaysTransactions);
--- part1/src/main.ts
+++ part1/src/main.ts
@@ -69,7 +69,7 @@ let z = <three>"main";
 //+++++++++++++++++++++++++++++++++++++++++ classes
 
 class Coder {
-   name: String;
+  name: String;
   age: number;
   location: string;
 
@@ -84,3 +84,130 @@ class Coder {
 
 
 let coder = new Coder("ivan", 23, "kajj")
+
+
+class WebDev extends Coder {
+  
+  constructor(public name:string, age: number, location:string, public laptopbrand:string ) {
+    super(name, age, location)
+    this.laptopbrand = laptopbrand
+  }
+}
+
+
+//+++++++++++++++++++++++++++++++++++++++++ implementing an interface to a class
+
+interface Musician {
+  name: string
+  instrument: string
+  play(actionn: string) : string
+}
+
+////////////////////////////////////////////////////////
+
+class Keyboardist implements Musician {
+  name: string;
+  instrument: string;
+  play(actionn: string): string {
+    return `${this.name} ${actionn} ${this.instrument}`
+  }
+
+  constructor(name:string, instrument:string, ) {
+    this.name= name
+    this.instrument = instrument
+  }
+}
+
+///////////////////////////////////////////////////////////
+
+const Page = new Keyboardist('mukisa', 'keyboard')
+console.log(Page.play('hits'));
+
+
+class Peeps {
+  static count: number = 0;  //static means that we can only call count after referencing this class forexample Peeps.count
+  static getCount(): number {
+    return Peeps.count
+  }
+  public id: number 
+  constructor(public name: string) {
+    this.name = name
+    this.id = ++Peeps.count
+  }
+}
+
+let John = new Peeps("John")
+let Lenon = new Peeps("John")
+let Bill = new Peeps("John")
+
+console.log(John.id);
+console.log(Peeps.count);
+
+///////////////////////////////////////////////////////////
+
+class Bands {
+  private datastate: string[]
+  constructor() {
+    this.datastate = []
+  }
+
+  public get data():string[] {
+    return this.datastate
+  }
+
+  public set data(value:string[]) {
+    if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+      this.datastate = value
+      return
+    } else {
+      throw new Error("some values in this array are not strings")
+    }
+  }
+}
+
+const MyBands = new Bands()
+MyBands.data = ['goo', 'lee', 'goo'] // -------------->setter
+console.log(MyBands.data);    // --------------> getter
+MyBands.data = [...MyBands.data, 'sisis']
+console.log(MyBands.data);
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++ index signatures and key of assertions
+
+//these are used when you are creating an object but you dont know the exact name of the object
+
+/*
+interface TransactionObj {
+  Pizza: number
+  Books: number
+  Job: number
+}
+*/
+
+interface TransactionObj {
+  [index: string] : number
+}
+
+const todaysTransactions: TransactionObj = {
+  Pizza: 10,
+  Books: 10,
+  Job: 20
+}
+
+let prop: string = 'Pizza'
+console.log(todaysTransactions[prop]);
+
+function todaysNet(transactions: TransactionObj) {
+  let total = 0
+  for (const transaction in transactions) {
+   total += transactions[transaction]
+  }
+  return total
+}
+console.log(todaysNet(todaysTransactions));
+
+//chaging the property of the object forexample 
+todaysTransactions.Pizza = 20
+console.log(todaysTransactions);
+
